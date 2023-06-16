c3e43675d692119100500be8c8779bc076029157
Emilio Romo <natsutsu04@gmail.com>
2023-06-15 21:38:18 -0600

    feat:Game v. 0.2 have a function for the button exit and a menu to choose your character!

--- game_RPG.py
+++ game_RPG.py
@@ -6,3 +6,17 @@ def show_InitialMenu():
     print("2. Exit\n")
     option = input("Enter your option:\n")
     return option
+
+def goodbye_Message():
+    print("Thanks for playing. See ya!")
+
+def show_CharacterMenu():
+    print("✩----------------------✩")
+    print("Select your character:")
+    print("1. Knight")
+    print("2. Wizard")
+    print("3. Paladin")
+    print("4. Alchemist")
+    option = input("Enter your option: ")
+    print("✩----------------------✩")
+    return option
