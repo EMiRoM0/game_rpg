8df4820fda1e5bbbf48affbf03884668a94afa28
Emilio Romo <natsutsu04@gmail.com>
2023-06-15 21:33:52 -0600

    feat:Game v. 0.1 have an initial menu now!

---
+++ game_RPG.py
@@ -0,0 +1,8 @@
+import random
+
+def show_InitialMenu():
+    print("Welcome to FinalPythonsy!\n")
+    print("1. Start Game")
+    print("2. Exit\n")
+    option = input("Enter your option:\n")
+    return option
